f4038e18e971bdee401aad3a420362980ab4ca44
Pranav-Talmale <pranavtalmale@gmail.com>
2024-01-05 11:42:52 +0530

    Add server address for the auth to work

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({baseUrl: ''});
+const baseQuery = fetchBaseQuery({baseUrl: 'https://lively-sky-64606.pktriot.net'});
 
 export const apiSlice = createApi({
     baseQuery,
