d5a2cf3aec08879f2f4db4b6d0e43eff0849ddfd
Pranav-Talmale <pranavtalmale@gmail.com>
2024-01-05 16:37:14 +0530

    Add glass-like effect on timeline cards and adjust registration criteria text

--- src/views/home/HomeSections/Register.js
+++ src/views/home/HomeSections/Register.js
@@ -15,34 +15,34 @@ export default function Register() {
       <img alt="..." className="path" src={require("assets/img/path1.png")} />
       <Container>
         <Row className="justify-content-md-center">
-          <Col className="text-center" lg="8" md="12">
-            <h2 className="title">
+          <Col lg="8" md="12">
+            <h2 className="title text-center">
             Registration Criteria
             </h2>
-            <h4 className="description">
-            1. Only <b>UNDERGRADUATE STUDENTS</b> can participate in the hackathon.
-            <br></br>
-            <br></br>
-            2. This competition is open for all irrespective of their branch, year of study etc.
-            <br></br>
-            <br></br>
-            3. Once the participants have registered for the event , they have to meet the requirements for ROUND 1 of the hackathon.
-            <br></br>
-            <br></br>
-            4. The presentation template provided by RAIT ACM has to be followed strictly followed by all the teams. Failing to do so will result in disqualification.
-            <br></br>
-            <br></br>
-            5. Only students who will pass the round 1 of the hackathon will be eligible for round 2. Arrangment of student accomadation is made in college. If student want to stay with parents they can stay at their own expense.
-            <br></br>
-            <br></br>
-            6. Students can work in teams where each team would have maximum of 4 members.
-            <br></br>
-            <br></br>
-            7. Submissions should be made on time. Late submissions will not be taken into account.
-            <br></br>
-            <br></br>
-            8. No plagiarism or reusing of past work will be allowed. If you decide to submit projects consisting of re-used code, or re-submit a project that you have already submitted previously to any other hackathon, you are to disclose such previous use and its extent with the submission. If not , their submissions will be dismissed.
-            </h4>
+              <p>
+                1. Only <b>UNDERGRADUATE STUDENTS</b> can participate in the hackathon.
+                <br></br>
+                <br></br>
+                2. This competition is open for all irrespective of their branch, year of study etc.
+                <br></br>
+                <br></br>
+                3. Once the participants have registered for the event , they have to meet the requirements for ROUND 1 of the hackathon.
+                <br></br>
+                <br></br>
+                4. The presentation template provided by RAIT ACM has to be followed strictly followed by all the teams. Failing to do so will result in disqualification.
+                <br></br>
+                <br></br>
+                5. Only students who will pass the round 1 of the hackathon will be eligible for round 2. Arrangment of student accomadation is made in college. If student want to stay with parents they can stay at their own expense.
+                <br></br>
+                <br></br>
+                6. Students can work in teams where each team would have maximum of 4 members.
+                <br></br>
+                <br></br>
+                7. Submissions should be made on time. Late submissions will not be taken into account.
+                <br></br>
+                <br></br>
+                8. No plagiarism or reusing of past work will be allowed. If you decide to submit projects consisting of re-used code, or re-submit a project that you have already submitted previously to any other hackathon, you are to disclose such previous use and its extent with the submission. If not , their submissions will be dismissed.
+              </p>
           </Col>
           <Col className="text-center" lg="8" md="12">
             <Button
