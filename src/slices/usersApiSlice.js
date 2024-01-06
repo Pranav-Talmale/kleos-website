026fa3e004a9c2a4fb393648664478029d366c28
Pranav-Talmale <pranavtalmale@gmail.com>
2024-01-06 17:39:15 +0530

    Slices: usersAPI: Add mutation to update user data

--- src/slices/usersApiSlice.js
+++ src/slices/usersApiSlice.js
@@ -22,12 +22,20 @@ export const userApiSlice = apiSlice.injectEndpoints({
           method: 'POST',
           body: data,
         }),
-      })
+      }),
+      updateUser: builder.mutation({
+        query: (data) => ({
+          url: `${USERS_URL}/profile`,
+          method: 'PUT',
+          body: data,
+        }),
+      }),
   }),
 });
 
 export const { 
   useLoginMutation, 
   useLogoutMutation, 
-  useRegisterMutation 
+  useRegisterMutation,
+  useUpdateUserMutation
 } = userApiSlice;
