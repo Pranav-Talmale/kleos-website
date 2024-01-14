5f7b3182f77421bfafc820f8266c09e91f8d317e
Pranav-Talmale <pranavtalmale@gmail.com>
2024-01-14 23:12:20 +0530

    Cleanup navbar leftovers

--- src/components/Navbars/ExamplesNavbar.js
+++ src/components/Navbars/ExamplesNavbar.js
@@ -100,7 +100,7 @@ export default function ExamplesNavbar() {
             <Row>
               <Col className="collapse-brand" xs="6">
                 <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  BLK•React
+                  KLEOS • By RAIT ACM
                 </a>
               </Col>
               <Col className="collapse-close text-right" xs="6">
